74584f29b12f48f50f32adcc6423cc53a07fac80
Javlonbek9393 <44919149+Javlonbek999@users.noreply.github.com>
2019-04-30 13:38:02 +0500

    committest

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+    	$this -> call ( PostTableSeeder:: class );
+        $this -> call ( TagTableSeeder:: class );
+
         // $this->call(UsersTableSeeder::class);
     }
 }
